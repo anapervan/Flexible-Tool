2d37a86bea4471a7504a5d16b372566d89683c5e
Bowen Feng <bowenfeng2021@u.northwestern.edu>
2021-05-10 00:01:26 -0500

    nonlinear differential system can be solved if we have apporiate initial values

--- 3D_optimization.py
+++ 3D_optimization.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[11]:
 
 
 from gekko import GEKKO
@@ -16,68 +16,142 @@ get_ipython().run_line_magic('matplotlib', 'inline')
 # In[ ]:
 
 
-# define u and mu as functions
-# t, c1, c2,c3 = symbols(r't, c1,c2,c3')
-# mu1 = Function(r'\mu1')(t)
-# mu2 = Function(r'\mu2')(t)
-# mu3 = Function(r'\mu3')(t)
-# mu4 = Function(r'\mu4')(t)
-# mu5 = Function(r'\mu5')(t)
-# mu6 = Function(r'\mu6')(t)
-# u1 = Function(r'\u1')(t)
-# u2 = Function(r'\u2')(t)
-# u3 = Function(r'\u3')(t)
 
-# mu1dot = mu1.diff(t)
-# mu2dot = mu2.diff(t)
-# mu3dot = mu3.diff(t)
-# mu4dot = mu4.diff(t)
-# mu5dot = mu5.diff(t)
-# mu6dot = mu6.diff(t)
 
 
 # In[ ]:
 
 
-# eq1 = Eq(u1 - c1**(-1)*mu1, 0)
-# eq2 = Eq(u2 - c2**(-1)*mu2, 0)
-# eq3 = Eq(u3 - c3**(-1)*mu3, 0)
-
-# eq4 = Eq(mu1dot - u3*mu3 + u2*mu3, 0)
-# eq5 = Eq(mu2dot - mu6 - u1*mu3 + u3*mu1, 0)
-# eq6 = Eq(mu3dot + mu5 - u2*mu1 + u1*mu2, 0)
-# eq7 = Eq(mu4dot - u3*mu5 + u2*mu6, 0)
-# eq8 = Eq(mu5dot - u1*mu6 + u3*mu4, 0)
-# eq9 = Eq(mu6dot - u2*mu4 + u1*mu5, 0)
-
-# soln = solve((eq1,eq2,eq3,eq4,eq5,eq6,eq7,eq8,eq9), (u1, u2, u3, mu1, mu2, mu3, mu4, mu5, mu6))
-# display(soln)
 
 
-# In[20]:
+
+# In[ ]:
+
+
+
+
+
+# In[64]:
 
 
 m = GEKKO() # initialize gekko
 nt = 101
 m.time = np.linspace(0,2,nt)
-# b = 0.75
-# Variables (initial values for mu)
-mu1 = m.Var(value=0, lb = 0, ub = 1) 
-mu2 = m.Var(value=0, lb = 0, ub = 1) 
-mu3 = m.Var(value=0, lb = 0, ub = 1) 
-mu4 = m.Var(value = 0, lb = 0, ub = 1)
-mu5 = m.Var(value=0, lb = 0, ub = 1) 
-mu6 = m.Var(value = 0, lb = 0, ub = 1)
+b = 0.75
+# Variables (initial values for mu), need to figure out what's the initial values for mus
+mu1 = m.Var(value=0.1, lb = 0, ub = 1) 
+mu2 = m.Var(value=0.1, lb = 0, ub = 1) 
+mu3 = m.Var(value=0.1, lb = 0, ub = 1) 
+mu4 = m.Var(value = 0.1, lb = 0, ub = 1)
+mu5 = m.Var(value=0.1, lb = 0, ub = 1) 
+mu6 = m.Var(value = 0.1, lb = 0, ub = 1)
+
 u1 = m.Var(value=0)
 u2 = m.Var(value=0) 
 u3 = m.Var(value=0) 
 
+# q1 = m.Var(value=0, lb = 0, ub = b) # x
+# q2 = m.Var(value=0, lb = 0, ub = b) # y
+# q3 = m.Var(value=0, lb = 0, ub = b) # z
+# q4 = m.Var(value = 0, lb = 0, ub = b) # theta1
+# q5 = m.Var(value=0, lb = 0, ub = b)  # theta2
+# q6 = m.Var(value = 0, lb = 0, ub = b) # theta3
+q11 = m.Var(value=0, lb = 0, ub = b)
+q12 = m.Var(value=0, lb = 0, ub = b)
+q13 = m.Var(value=0, lb = 0, ub = b)
+q14 = m.Var(value=0, lb = 0, ub = b)
+q21 = m.Var(value=0, lb = 0, ub = b)
+q22 = m.Var(value=0, lb = 0, ub = b)
+q23 = m.Var(value=0, lb = 0, ub = b)
+q24 = m.Var(value=0, lb = 0, ub = b)
+q31 = m.Var(value=0, lb = 0, ub = b)
+q32 = m.Var(value=0, lb = 0, ub = b)
+q33 = m.Var(value=0, lb = 0, ub = b)
+q34 = m.Var(value=0, lb = 0, ub = b)
+q41 = m.Var(value=0, lb = 0, ub = b)
+q42 = m.Var(value=0, lb = 0, ub = b)
+q43 = m.Var(value=0, lb = 0, ub = b)
+q44 = m.Var(value=0, lb = 0, ub = b)
+
+x1 = [[0, 0, 0, 0], 
+    [0, 0, -1, 0],
+    [0, 1, 0, 0],
+     [0, 0, 0, 0]]
+
+x2 = [[0, 0, 1, 0], 
+    [0, 0, 0, 0],
+    [-1, 0, 0, 0],
+     [0, 0, 0, 0]]
+
+x3 = [[0, -1, 0, 0], 
+    [1, 0, 0, 0],
+    [0, 1, 0, 0],
+     [0, 0, 0, 0]]
+
+x4 = [[0, 0, 0, 1], 
+    [0, 0, 0, 0],
+    [0, 1, 0, 0],
+     [0, 0, 0, 0]]
+
+x5 = [[0, 0, 0, 0], 
+    [0, 0, 0, 1],
+    [0, 1, 0, 0],
+     [0, 0, 0, 0]]
+
+x6 = [[0, 0, 0, 0], 
+    [0, 0, 0, 0],
+    [0, 0, 0, 1],
+     [0, 0, 0, 0]]
+
+
+# In[65]:
+
+
+# variable array dimension
+n = 4 # rows
+p = 4 # columns
+# create array
+q = m.Array(m.Var,(n,p))
+for i in range(n):
+    for j in range (n):
+            q[i,j].value = 2.0
+
+qminimize = m.Var(value = 0)
+
+
+# In[66]:
+
+
+X1 = m.Array(m.Const, (n,p))
+X1[1, 2] = -1
+X1[2, 1] = 1
+display(X1)
+X2 = m.Array(m.Const, (n,p))
+X2[2, 0] = -1
+X2[0, 2] = 1
+display(X2)
+X3 = m.Array(m.Const, (n,p))
+X3[0, 1] = -1
+X3[1, 0] = 1
+display(X3)
+X4 = m.Array(m.Const, (n,p))
+X4[0, 3] = 1
+display(X4)
+
+
+# In[ ]:
+
+
+
+
+
+# In[67]:
 
 
 # Equations
-c1 = 0.5
-c2 = 0.5
-c3 = 0.5
+c1 = 1.0
+c2 = 1.0
+c3 = 1.0
 u1 = c1**(-1)*mu1
 u2 = c2**(-1)*mu2
 u3 = c3**(-1)*mu3
@@ -87,38 +161,71 @@ m.Equation(mu3.dt()== -mu5 + u2*mu1 - u1*mu2)
 m.Equation(mu4.dt()== u3*mu5 - u2*mu6)
 m.Equation(mu5.dt()== u1*mu6 - u3*mu4)
 m.Equation(mu6.dt()== u2*mu4 - u1*mu5)
+
+
+m.Equation(q11.dt() == u3*q[0,1] - u2*q[0,2])
+m.Equation(q12.dt() == -u3*q[0,0] + u1*q[0,2])
+m.Equation(q13.dt() == u2*q[0,0] - u1*q[0,1])
+m.Equation(q14.dt() == q[0,0])
+
+m.Equation(q21.dt() == u3*q[1,1] - u2*q[1,2])
+m.Equation(q22.dt() == -u3*q[1,0] + u1*q[1,2])
+m.Equation(q23.dt() == u2*q[1,0] - u1*q[1,1])
+m.Equation(q24.dt() == q[1,0])
+
+m.Equation(q31.dt() == u3*q[2,1] - u2*q[2,2])
+m.Equation(q32.dt() == -u3*q[2,0] + u1*q[2,2])
+m.Equation(q33.dt() == u2*q[2,0] - u1*q[2,1])
+m.Equation(q34.dt() == q[2,0])
+
+m.Equation(q41.dt() == u3*q[3,1] - u2*q[3,2])
+m.Equation(q42.dt() == -u3*q[3,0] + u1*q[3,2])
+m.Equation(q43.dt() == u2*q[3,0] - u1*q[3,1])
+m.Equation(q44.dt() == q[3,0])
+
+
+m.Equation(qminimize.dt()== 0.5*(c1*u1**2 + c2*u2**2 + c3*u3**2))
+
+
+
+
+# In[68]:
+
+
+# set final time will be 1
+p = np.zeros(nt) # mark final time point
+p[-1] = 1.0
+final = m.Param(value=p)
+
+m.Obj(qminimize*final)
+
+
+
+
 m.options.IMODE = 6
 m.solve(disp=False)
 
 
+# In[ ]:
 
 
 
-# In[21]:
 
 
-print('Results')
-print('u1: ' + str(u1.value))
-print('u2: ' + str(u2.value))
-print('u3: ' + str(u3.value))
-print('mu1: ' + str(mu1.value))
-
-plt.figure(1) # plot results
-plt.plot(m.time,mu1.value,'k-',label=r'$mu_1$')
-plt.plot(m.time,mu2.value,'b-',label=r'$mu_2$')
-plt.plot(m.time,mu3.value,'g-',label=r'$mu_3$')
-plt.plot(m.time,mu4.value,'y-',label=r'$mu_4$')
-plt.plot(m.time,mu5.value,'r--',label=r'$mu_5$')
+# In[ ]:
 
 
-# In[12]:
 
 
-from scipy.integrate import solve_ivp
-import numpy as np
+
+# In[8]:
 
 
-# In[22]:
+# from scipy.integrate import solve_ivp
+# import numpy as np
+
+
+# In[9]:
 
 
 # c1 = 0.5
@@ -144,6 +251,12 @@ import numpy as np
 #              v[6]*v[5] - v[8]*v[3],
 #              v[7]*v[3] - v[6]*v[4]]
 # res = solve_ivp(rhs, (0, 2), [0.1, 0.1, 0.1,0.1,0.1,0.1, 0.1, 0.1, 0.1])
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
